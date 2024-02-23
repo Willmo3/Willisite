99faded9fadfd89ad09accd420d38faa848dac50
Willmo3 <thelordofbouge@gmail.com>
2024-02-23 11:30:06 -0500

    Add mobile nav menu

--- js/nav.js
+++ js/nav.js
@@ -2,14 +2,19 @@ let toggled = false;
 const dropdown = document.getElementById("dropdown");
 const img = dropdown.getElementsByTagName('img')[0];
 const nav = document.getElementsByTagName('nav')[0];
+const lis = nav.getElementsByTagName('ul')[0].children;
 
 dropdown.addEventListener('click', () => {
     toggled = !toggled;
     if (toggled) {
         img.src = 'img/crossed.svg';
-        nav.classList.remove('mobile:hidden');
+        nav.classList.add('mobile:h-36');
+        nav.classList.remove('mobile:h-0');
+        nav.classList.remove('mobile:opacity-0');
     } else {
         img.src = 'img/hamburger.svg';
-        nav.classList.add('mobile:hidden');
+        nav.classList.add('mobile:h-0');
+        nav.classList.remove('mobile:h-36');
+        nav.classList.add('mobile:opacity-0');
     }
 });
