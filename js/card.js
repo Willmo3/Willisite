3e0c8ac054a776b88ecece2ca29a35448ab11c5c
Willmo3 <thelordofbouge@gmail.com>
2024-02-23 12:11:05 -0500

    Add WillZip

--- js/card.js
+++ js/card.js
@@ -1,3 +1,11 @@
+const img_width = 'w-60';
+const img_height = 'h-48';
+const img_margin = 'm-5';
+
+const no_width = 'w-0';
+const no_height = 'h-0';
+
+
 class Card {
     constructor(element) {
         this.element = element;
@@ -13,13 +21,17 @@ class Card {
     enable() {
         this.chevron.src = 'img/chevron-up.svg';
 
-        this.img.classList.remove('w-0');
+        this.img.classList.remove(no_width);
+        this.img.classList.remove(no_height);
+
         this.text.classList.remove('max-h-32');
         this.element.classList.remove('max-h-48');
         this.header.classList.remove('h-14');
 
-        this.img.classList.add('m-5');
-        this.img.classList.add('w-48');
+        this.img.classList.add(img_margin);
+        this.img.classList.add(img_width);
+        this.img.classList.add(img_height);
+
         this.text.classList.add('max-h-72');
         this.header.classList.add('h-20');
     }
@@ -27,12 +39,16 @@ class Card {
     disable() {
         this.chevron.src = 'img/chevron-down.svg';
 
-        this.img.classList.remove('m-5');
-        this.img.classList.remove('w-48');
+        this.img.classList.remove(img_margin);
+        this.img.classList.remove(img_width);
+        this.img.classList.remove(img_height);
+        
         this.text.classList.remove('max-h-72');
         this.header.classList.remove('h-20');
 
-        this.img.classList.add('w-0');
+        this.img.classList.add(no_width);
+        this.img.classList.add(no_height);
+
         this.text.classList.add('max-h-32');
         this.element.classList.add('max-h-48');
         this.header.classList.add('h-14');
