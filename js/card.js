a5ade3420a89cf58b0ec3ff392a58c0e0c1d81ea
Willmo3 <thelordofbouge@gmail.com>
2024-02-23 12:40:57 -0500

    Remove unneeded max text

--- js/card.js
+++ js/card.js
@@ -1,6 +1,6 @@
-const img_width = 'w-60';
+const img_width = 'w-72';
 const img_height = 'h-48';
-const img_margin = 'm-5';
+const img_margin = 'm-4';
 
 const no_width = 'w-0';
 const no_height = 'h-0';
@@ -14,7 +14,6 @@ class Card {
         this.content = element.getElementsByClassName('content')[0];
 
         this.img = element.getElementsByClassName('pic')[0];
-        this.text = element.getElementsByClassName('text')[0];
         this.chevron = this.header.getElementsByTagName('img')[0];
     }
 
@@ -24,15 +23,12 @@ class Card {
         this.img.classList.remove(no_width);
         this.img.classList.remove(no_height);
 
-        this.text.classList.remove('max-h-32');
-        this.element.classList.remove('max-h-48');
         this.header.classList.remove('h-14');
 
         this.img.classList.add(img_margin);
         this.img.classList.add(img_width);
         this.img.classList.add(img_height);
 
-        this.text.classList.add('max-h-72');
         this.header.classList.add('h-20');
     }
 
@@ -43,14 +39,10 @@ class Card {
         this.img.classList.remove(img_width);
         this.img.classList.remove(img_height);
         
-        this.text.classList.remove('max-h-72');
         this.header.classList.remove('h-20');
 
         this.img.classList.add(no_width);
         this.img.classList.add(no_height);
-
-        this.text.classList.add('max-h-32');
-        this.element.classList.add('max-h-48');
         this.header.classList.add('h-14');
     }
 }
