58f8628954c1a8b2d7717324ab917c2e0a4ced0a
Willmo3 <thelordofbouge@gmail.com>
2024-02-23 11:18:47 -0500

    Add dropdown

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,15 @@
 let toggled = false;
 const dropdown = document.getElementById("dropdown");
 const img = dropdown.getElementsByTagName('img')[0];
+const nav = document.getElementsByTagName('nav')[0];
 
 dropdown.addEventListener('click', () => {
     toggled = !toggled;
     if (toggled) {
         img.src = 'img/crossed.svg';
+        nav.classList.remove('mobile:hidden');
     } else {
         img.src = 'img/hamburger.svg';
+        nav.classList.add('mobile:hidden');
     }
 });
